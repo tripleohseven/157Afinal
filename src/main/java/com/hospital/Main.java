7a8568f2f2900ed450e636e286d2d3ebf243d93e
Niki Liao <118552370+nikiliao25@users.noreply.github.com>
2025-05-06 17:49:58 -0700

    Add files via upload

---
+++ src/main/java/com/hospital/Main.java
@@ -0,0 +1,23 @@
+package com.hospital;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Login.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        primaryStage.setTitle("Hospital Management - Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
